fe2f6b708abe2f1bc90b6e84e162a3db25630f46
Christian <xianlanoue@gmail.com>
2017-10-05 13:37:16 -0400

    fixed event handlers in createform

--- src/components/Forms/CreateForm.js
+++ src/components/Forms/CreateForm.js
@@ -1,29 +1,40 @@
 import React, {Component} from 'react'
-import ReactDOM from 'react-dom'
 import axios from 'axios'
 
 class CreateForm extends Component {
   constructor(props) {
     super(props);
-    this.state = {title: '',
-                  year: '',
-                  director: '',
-                  photo_url: ''};
+    this.state = {
+      movie: {
+        title: '',
+        year: '',
+        director: '',
+        photo_url: ''
+      }
+    }
 
-    this.handleCreate = this.handleCreate.bind(this);
+    this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleCreate(e) {
-    this.setState({title: e.target.title,
-                   year: e.target.year,
-                   director: e.target.director,
-                   photo_url: e.target.photo_url});
+  handleChange(e) {
+    console.log(e.target);
+    // this.setState({title: e.target.title,
+    //                year: e.target.year,
+    //                director: e.target.director,
+    //                photo_url: e.target.photo_url});
+    this.setState({
+      movie:{
+        ...this.state.movie,
+        [e.target.name]:e.target.value
+      }
+    }, ()=>console.log(this.state))
   }
 
   handleSubmit(e) {
-    alert('Movie was submitted: ');
+    alert('Movie was submitted');
     e.preventDefault();
+    axios.post("/movies", this.state)
   }
 
   render() {
@@ -31,19 +42,19 @@ class CreateForm extends Component {
       <form onSubmit={this.handleSubmit}>
         <label>
           Title:
-          <input type="text" value={this.state.title} onCreate={this.handleCreate} />
+          <input name="title" type="text" value={this.state.title} onChange={this.handleChange} />
         </label>
         <label>
           Year:
-          <input type="text" value={this.state.year} onCreate={this.handleCreate} />
+          <input name="year" type="text" value={this.state.year} onChange={this.handleChange} />
         </label>
         <label>
           Director:
-          <input type="text" value={this.state.director} onCreate={this.handleCreate} />
+          <input name="director" type="text" value={this.state.director} onChange={this.handleChange} />
         </label>
         <label>
           Photo:
-          <input type="text" value={this.state.photo_url} onCreate={this.handleCreate} />
+          <input name="photo_url" type="text" value={this.state.photo_url} onChange={this.handleChange} />
         </label>
         <input type="submit" value="Submit" />
       </form>
--- src/components/Forms/UpdateForm.js
+++ src/components/Forms/UpdateForm.js
@@ -1,20 +1,23 @@
 import React, {Component} from 'react'
-import ReactDOM from 'react-dom'
-import axios from 'axios'
 
 class UpdateForm extends Component {
   constructor(props) {
     super(props)
-    this.state = {title: '',
-                  year: '',
-                  director: '',
-                  photo_url: ''};
 
-    this.handleUpdate = this.handleUpdate.bind(this);
+    this.state = {
+      movie: {
+        title: '',
+        year: '',
+        director: '',
+        photo_url: ''
+      }
+    }
+
+    this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleUpdate(e) {
+  handleChange(e) {
     this.setState ({title: e.target.title,
                     year: e.target.year,
                     director: e.target.director,
@@ -31,19 +34,19 @@ class UpdateForm extends Component {
       <form onSubmit={this.handleSubmit}>
         <label>
           Title:
-          <input type="text" value={this.state.title} onUpdate={this.handleUpdate} />
+          <input type="text" value={this.state.title} onChange={this.handleChange} />
         </label>
         <label>
           Year:
-          <input type="text" value={this.state.year} onUpdate={this.handleUpdate} />
+          <input type="text" value={this.state.year} onChange={this.handleChange} />
         </label>
         <label>
           Director:
-          <input type="text" value={this.state.director} onUpdate={this.handleUpdate} />
+          <input type="text" value={this.state.director} onChange={this.handleChange} />
         </label>
         <label>
           Photo:
-          <input type="text" value={this.state.photo_url} onUpdate={this.handleUpdate} />
+          <input type="text" value={this.state.photo_url} onChange={this.handleChange} />
         </label>
         <input type="submit" value="Submit" />
       </form>
